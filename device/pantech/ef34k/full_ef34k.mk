8aa07f4fa2f599643d983b798042894f55e8dcb4
wangsai008 <wangsai_008@163.com>
2013-05-09 14:29:02 +0800

    first commit

---
+++ device/pantech/ef34k/full_ef34k.mk
@@ -0,0 +1,13 @@
+
+# Release name
+PRODUCT_RELEASE_NAME := ef34k
+
+# Inherit device configuration
+$(call inherit-product, device/pantech/ef34k/device_ef34k.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ef34k
+PRODUCT_NAME := full_ef34k
+PRODUCT_BRAND := pantech
+PRODUCT_MODEL := ef34k
+PRODUCT_MANUFACTURER := pantech
